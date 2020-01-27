b8d8fb070fa0704befd33f3f59af0c170b7b24f2
Alex Martinez <alexandr3martin3z@gmail.com>
2020-01-27 23:41:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,5 +5,20 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+
+      erb :new
+    end
+
+    post '/pirates' do
+
+
+      erb :show
+    end
+
   end
 end
